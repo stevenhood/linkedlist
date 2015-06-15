24e589ae0e1e5182e3f801e9b76f02f11c4e9a01
Steven Hood <steven.hood@live.co.uk>
2015-06-15 16:18:03 +0100

    Updated Main to use Hungarian notation in variable names

--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    LinkedList list;
-    list.add(42);
-    list.add(15);
-    list.add(14);
-    list.add(29);
-    list.print();
+    LinkedList oList;
+    oList.add(42);
+    oList.add(15);
+    oList.add(14);
+    oList.add(29);
+    oList.print();
     return 0;
 }

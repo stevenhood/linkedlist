b435270c187811da0bb236f46798e765c7fb50b4
Steven Hood <steven.hood@live.co.uk>
2015-06-15 13:59:25 +0100

    Initial Node class definition

---
+++ Node.h
@@ -0,0 +1,12 @@
+class Node
+{
+public:
+    int getValue() const { return value; }
+    void setValue(int n) { value = n; }
+    int getNext() const { return next; }
+    void setNext(Node *node) { next = node; }
+
+private:
+    int value;
+    Node *next;
+}
